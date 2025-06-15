6974c30ff65f96aca7097771337545287a0d5817
kaswhy <sxshx818@naver.com>
2025-06-15 15:31:47 +0900

    chore(*): 프로젝트 세팅

---
+++ src/main/java/org/sopt36/ninedotserver/NinedotServerApplication.java
@@ -0,0 +1,13 @@
+package org.sopt36.ninedotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinedotServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinedotServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sopt36/ninedotserver/NinedotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt36.ninedotserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinedotServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
