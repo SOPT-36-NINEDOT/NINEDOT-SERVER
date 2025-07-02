db795b23b025e3ee6563c1f7d6ea4da6e845fc1c
kaswhy <sxshx818@naver.com>
2025-07-02 21:44:57 +0900

    :bug: fix(#9/security): security config에 annotation 추가

--- src/main/java/org/sopt36/ninedotserver/global/config/security/SecurityConfig.java
+++ src/main/java/org/sopt36/ninedotserver/global/config/security/SecurityConfig.java
@@ -1,9 +1,11 @@
 package org.sopt36.ninedotserver.global.config.security;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +14,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
