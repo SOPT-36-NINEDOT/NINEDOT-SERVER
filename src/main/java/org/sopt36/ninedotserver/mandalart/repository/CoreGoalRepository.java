d31eb6954c412c2d084f9369342ec57989e05c0f
kaswhy <sxshx818@naver.com>
2025-07-07 18:38:04 +0900

    :sparkles: feat(#14/mandalart): 만다라트 관련 리포지토리 작성

---
+++ src/main/java/org/sopt36/ninedotserver/mandalart/repository/CoreGoalRepository.java
@@ -0,0 +1,9 @@
+package org.sopt36.ninedotserver.mandalart.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoreGoalRepository extends JpaRepository<CoreGoalRepository, Long> {
+
+}
