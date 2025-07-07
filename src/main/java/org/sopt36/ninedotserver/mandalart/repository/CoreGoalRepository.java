60c31b66b0234a2f98dd579596026075c465152b
kaswhy <sxshx818@naver.com>
2025-07-07 19:19:49 +0900

    :bug: fix(#14/mandalart): JpaRepository 제네릭 타입 오류 수정

--- src/main/java/org/sopt36/ninedotserver/mandalart/repository/CoreGoalRepository.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/repository/CoreGoalRepository.java
@@ -1,10 +1,11 @@
 package org.sopt36.ninedotserver.mandalart.repository;
 
+import org.sopt36.ninedotserver.mandalart.domain.CoreGoal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CoreGoalRepository
-    extends JpaRepository<CoreGoalRepository, Long>, CoreGoalRepositoryCustom {
+    extends JpaRepository<CoreGoal, Long>, CoreGoalRepositoryCustom {
 
 }
