3d4f97ec9befde901b9e34aa7b23b7a4c7b8b001
kaswhy <sxshx818@naver.com>
2025-07-07 17:48:22 +0900

    :sparkles: feat(#14/onboarding): 온보딩 시 질문 관련 도메인 생성

---
+++ src/main/java/org/sopt36/ninedotserver/onboarding/domain/Question.java
@@ -0,0 +1,21 @@
+package org.sopt36.ninedotserver.onboarding.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt36.ninedotserver.global.entity.BaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Question extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
