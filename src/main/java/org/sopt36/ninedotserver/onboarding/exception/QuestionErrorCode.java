21aa00a59c1b12cfd1acafac7770c9974185055c
kaswhy <sxshx818@naver.com>
2025-07-07 19:52:02 +0900

    :sparkles: feat(#14/onboarding): 온보딩 관련 errorCode 작성

---
+++ src/main/java/org/sopt36/ninedotserver/onboarding/exception/QuestionErrorCode.java
@@ -0,0 +1,25 @@
+package org.sopt36.ninedotserver.onboarding.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt36.ninedotserver.global.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum QuestionErrorCode implements ErrorCode {
+
+    // TODO 에러 코드 별 메시지 관리. 추후 구현 시 아래 세미콜론 삭제
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
