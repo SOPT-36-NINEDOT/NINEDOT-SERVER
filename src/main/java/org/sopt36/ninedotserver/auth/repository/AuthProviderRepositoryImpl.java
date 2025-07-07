eb211b392495b11048c678f00630a84fbe4fd5be
kaswhy <sxshx818@naver.com>
2025-07-07 19:25:27 +0900

    :bug: fix(#14/auth): 커스텀 리포지토리 구현 부분에 커스텀 인터페이스 구현 코드 작성

--- src/main/java/org/sopt36/ninedotserver/auth/repository/AuthProviderRepositoryImpl.java
+++ src/main/java/org/sopt36/ninedotserver/auth/repository/AuthProviderRepositoryImpl.java
@@ -4,7 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class AuthProviderRepositoryImpl {
+public class AuthProviderRepositoryImpl implements AuthProviderRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
