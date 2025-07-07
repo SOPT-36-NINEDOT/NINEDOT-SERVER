1feee47cc97d2d04813a9c677058ab9d0d8beb83
kaswhy <sxshx818@naver.com>
2025-07-07 18:14:25 +0900

    :sparkles: feat(#14/ai): ai 관련 서비스 작성

---
+++ src/main/java/org/sopt36/ninedotserver/ai/service/AiRecommendationService.java
@@ -0,0 +1,11 @@
+package org.sopt36.ninedotserver.ai.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AiRecommendationService {
+
+
+}
