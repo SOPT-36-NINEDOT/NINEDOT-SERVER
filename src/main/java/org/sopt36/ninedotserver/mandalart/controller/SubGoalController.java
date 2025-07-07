8867e32d484796fc0b7f77e550ec85d5735a64df
kaswhy <sxshx818@naver.com>
2025-07-07 19:28:23 +0900

    :bug: fix(#14/mandalart): 하위 목표 컨트롤러에 멤버 변수 final 빠진 부분 추가

--- src/main/java/org/sopt36/ninedotserver/mandalart/controller/SubGoalController.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/controller/SubGoalController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SubGoalController {
 
-    private SubGoalCommandService subGoalCommandService;
-    private SubGoalQueryService subGoalQueryService;
+    private final SubGoalCommandService subGoalCommandService;
+    private final SubGoalQueryService subGoalQueryService;
 
 }
