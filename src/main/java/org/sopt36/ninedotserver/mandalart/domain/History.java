6dc6e75129512fd213fac33a82c5bfa02a60128d
Jeon Chan Min <chanmin010702@gmail.com>
2025-07-08 15:20:37 +0900

    :sparkles: feat(#18/create-mandalart-entities): History 엔티티 기본 구조 생성

--- src/main/java/org/sopt36/ninedotserver/mandalart/domain/History.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/domain/History.java
@@ -1,9 +1,14 @@
 package org.sopt36.ninedotserver.mandalart.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,5 +22,20 @@ public class History extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subgoal_id", nullable = false)
+    private SubGoal subGoal;
+
+    @Column(nullable = false)
+    private LocalDate completedDate;
+
+    private History(SubGoal subGoal, LocalDate completedDate) {
+        this.subGoal = subGoal;
+        this.completedDate = completedDate;
+    }
+
+    public static History create(SubGoal subGoal, LocalDate completedDate) {
+        return new History(subGoal, completedDate);
+    }
 }
