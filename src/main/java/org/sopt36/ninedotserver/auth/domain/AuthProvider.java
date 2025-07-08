a7285a2d3afd38a45306c5ec2e89090c543cef68
lezgititt <bsj6734@gmail.com>
2025-07-08 18:55:52 +0900

    :sparkles: ✨ feat(#16/AuthProvider): AuthProvider 엔티티 기본 구조

--- src/main/java/org/sopt36/ninedotserver/auth/domain/AuthProvider.java
+++ src/main/java/org/sopt36/ninedotserver/auth/domain/AuthProvider.java
@@ -1,16 +1,28 @@
 package org.sopt36.ninedotserver.auth.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.sopt36.ninedotserver.global.entity.BaseEntity;
+import org.sopt36.ninedotserver.user.domain.User;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(access = AccessLevel.PROTECTED)
+@Table(name = "auth_provider")
 @Entity
 public class AuthProvider extends BaseEntity {
 
@@ -18,4 +30,19 @@ public class AuthProvider extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "provider", length = 10, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ProviderType provider;
+
+    @Column(name = "provider_user_id", length = 255, nullable = false, unique = true)
+    private String provider_user_id;
+
+    public static AuthProvider create(User user, ProviderType provider, String provider_user_id) {
+        return AuthProvider.builder().user(user).provider(provider)
+                   .provider_user_id(provider_user_id).build();
+    }
 }
