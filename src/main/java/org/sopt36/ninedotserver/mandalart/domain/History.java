fb1be8dd094c31b3fe05441a426284fe9c75cc0e
Jeon Chan Min <chanmin010702@gmail.com>
2025-07-08 19:38:14 +0900

    :art: feat(#18/create-mandalart-entities): History 엔티티 코드 컨벤션에 맞게 수정

--- src/main/java/org/sopt36/ninedotserver/mandalart/domain/History.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/domain/History.java
@@ -8,14 +8,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.sopt36.ninedotserver.global.entity.BaseEntity;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(access = AccessLevel.PROTECTED)
+@Table(name = "history")
 @Entity
 public class History extends BaseEntity {
 
@@ -27,15 +33,14 @@ public class History extends BaseEntity {
     @JoinColumn(name = "subgoal_id", nullable = false)
     private SubGoal subGoal;
 
-    @Column(nullable = false)
+    @Column(name = "completed_date", nullable = false)
     private LocalDate completedDate;
 
-    private History(SubGoal subGoal, LocalDate completedDate) {
-        this.subGoal = subGoal;
-        this.completedDate = completedDate;
-    }
-
     public static History create(SubGoal subGoal, LocalDate completedDate) {
-        return new History(subGoal, completedDate);
+        return History.builder()
+            .subGoal(subGoal)
+            .completedDate(completedDate)
+            .build();
+
     }
 }
