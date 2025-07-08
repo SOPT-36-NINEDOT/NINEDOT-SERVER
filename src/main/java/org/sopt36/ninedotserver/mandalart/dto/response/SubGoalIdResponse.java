4b97acf9b70dd0857b24c5008108c873e60bfab7
Jeon Chan Min <chanmin010702@gmail.com>
2025-07-09 00:13:23 +0900

    :art: feat(#30/subgoal-id-list-api): SubGoalIdResponse에 from 메서드 추가

--- src/main/java/org/sopt36/ninedotserver/mandalart/dto/response/SubGoalIdResponse.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/dto/response/SubGoalIdResponse.java
@@ -1,8 +1,14 @@
 package org.sopt36.ninedotserver.mandalart.dto.response;
 
+import org.sopt36.ninedotserver.mandalart.domain.SubGoal;
+
 public record SubGoalIdResponse(
     Long id,
     int position
 ) {
+
+    public static SubGoalIdResponse from(SubGoal subGoal) {
+        return new SubGoalIdResponse(subGoal.getId(), subGoal.getPosition());
+    }
 
 }
