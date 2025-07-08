a506e09a7e59f86ded8bae97efeda63f946a5fd4
Jeon Chan Min <chanmin010702@gmail.com>
2025-07-09 01:50:08 +0900

    :sparkles: feat(#30/subgoal-id-list-api): SubGoalQueryService 에서 getSubGoalIds 메서드 구현

--- src/main/java/org/sopt36/ninedotserver/mandalart/service/query/SubGoalQueryService.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/service/query/SubGoalQueryService.java
@@ -1,6 +1,14 @@
 package org.sopt36.ninedotserver.mandalart.service.query;
 
+import static org.sopt36.ninedotserver.mandalart.exception.SubGoalErrorCode.CORE_GOAL_NOT_FOUND;
+import static org.sopt36.ninedotserver.mandalart.exception.SubGoalErrorCode.FORBIDDEN_ACCESS;
+
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.sopt36.ninedotserver.mandalart.domain.CoreGoal;
+import org.sopt36.ninedotserver.mandalart.domain.SubGoal;
+import org.sopt36.ninedotserver.mandalart.dto.response.SubGoalIdResponse;
+import org.sopt36.ninedotserver.mandalart.exception.SubGoalException;
 import org.sopt36.ninedotserver.mandalart.repository.CoreGoalRepository;
 import org.sopt36.ninedotserver.mandalart.repository.MandalartRepository;
 import org.sopt36.ninedotserver.mandalart.repository.SubGoalRepository;
@@ -16,4 +24,18 @@ public class SubGoalQueryService {
     private final CoreGoalRepository coreGoalRepository;
     private final MandalartRepository mandalartRepository;
 
+    public List<SubGoalIdResponse> getSubGoalIds(Long userId, Long coreGoalId) {
+        CoreGoal coreGoal = coreGoalRepository.findById(coreGoalId)
+            .orElseThrow(() -> new SubGoalException(CORE_GOAL_NOT_FOUND));
+
+        if (!coreGoal.getMandalart().getUser().getId().equals(userId)) {
+            throw new SubGoalException(FORBIDDEN_ACCESS);
+        }
+
+        List<SubGoal> subGoals = subGoalRepository.findAllByCoreGoalId(coreGoalId);
+
+        return subGoals.stream()
+            .map(SubGoalIdResponse::from)
+            .toList();
+    }
 }
