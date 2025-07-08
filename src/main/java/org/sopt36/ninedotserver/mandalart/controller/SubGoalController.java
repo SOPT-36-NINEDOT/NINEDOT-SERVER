568329c7736d714a7f665fa78bf6a3b58f4588ae
Jeon Chan Min <chanmin010702@gmail.com>
2025-07-08 23:42:15 +0900

    :sparkles:   feat(#30/subgoal-id-list-api): SubGoalController 구현

--- src/main/java/org/sopt36/ninedotserver/mandalart/controller/SubGoalController.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/controller/SubGoalController.java
@@ -1,15 +1,38 @@
 package org.sopt36.ninedotserver.mandalart.controller;
 
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.sopt36.ninedotserver.global.dto.response.ApiResponse;
+import org.sopt36.ninedotserver.mandalart.dto.response.SubGoalIdResponse;
 import org.sopt36.ninedotserver.mandalart.service.command.SubGoalCommandService;
 import org.sopt36.ninedotserver.mandalart.service.query.SubGoalQueryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/core-goals")
 public class SubGoalController {
 
     private final SubGoalCommandService subGoalCommandService;
     private final SubGoalQueryService subGoalQueryService;
+
+    @GetMapping("/{coreGoalId}/sub-goals")
+    public ResponseEntity<ApiResponse<Map<String, List<SubGoalIdResponse>>, Void>> getSubGoalIds(
+        @PathVariable Long coreGoalId
+    ) {
+        Long userId = 1L;
+        List<SubGoalIdResponse> subGoalIds = subGoalQueryService.getSubGoalIds(userId, coreGoalId);
+        Map<String, List<SubGoalIdResponse>> response = Map.of("subGoalIds", subGoalIds);
+        return ResponseEntity.ok(
+            ApiResponse.ok("성공적으로 해당 상위 목표의 하위 목표 ID 리스트를 조회했습니다.", response)
+        );
+
+
+    }
 
 }
