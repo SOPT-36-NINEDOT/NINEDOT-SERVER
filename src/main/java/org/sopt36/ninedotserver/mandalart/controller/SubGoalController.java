e526663e628845ca431c802d247fee55099481b3
Jeon Chan Min <chanmin010702@gmail.com>
2025-07-10 05:18:58 +0900

    :sparkles: feat(#36/mandalart): SubGoalController updateSubGoal api 추가

--- src/main/java/org/sopt36/ninedotserver/mandalart/controller/SubGoalController.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/controller/SubGoalController.java
@@ -2,6 +2,7 @@ package org.sopt36.ninedotserver.mandalart.controller;
 
 import static org.sopt36.ninedotserver.mandalart.controller.message.SubGoalMessage.SUB_GOAL_CREATE_SUCCESS;
 import static org.sopt36.ninedotserver.mandalart.controller.message.SubGoalMessage.SUB_GOAL_ID_LIST_FETCH_SUCCESS;
+import static org.sopt36.ninedotserver.mandalart.controller.message.SubGoalMessage.SUB_GOAL_UPDATE_SUCCESS;
 
 import jakarta.validation.Valid;
 import java.net.URI;
@@ -9,6 +10,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.sopt36.ninedotserver.global.dto.response.ApiResponse;
 import org.sopt36.ninedotserver.mandalart.dto.request.SubGoalCreateRequest;
+import org.sopt36.ninedotserver.mandalart.dto.request.SubGoalUpdateRequest;
 import org.sopt36.ninedotserver.mandalart.dto.response.SubGoalCreateResponse;
 import org.sopt36.ninedotserver.mandalart.dto.response.SubGoalIdListResponse;
 import org.sopt36.ninedotserver.mandalart.dto.response.SubGoalIdResponse;
@@ -16,13 +18,14 @@ import org.sopt36.ninedotserver.mandalart.service.command.SubGoalCommandService;
 import org.sopt36.ninedotserver.mandalart.service.query.SubGoalQueryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/core-goals")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @RestController
 public class SubGoalController {
@@ -30,7 +33,7 @@ public class SubGoalController {
     private final SubGoalCommandService subGoalCommandService;
     private final SubGoalQueryService subGoalQueryService;
 
-    @GetMapping("/{coreGoalId}/sub-goals")
+    @GetMapping("/core-goals/{coreGoalId}/sub-goals")
     public ResponseEntity<ApiResponse<SubGoalIdListResponse, Void>> getSubGoalIds(
         @PathVariable Long coreGoalId
     ) {
@@ -44,7 +47,7 @@ public class SubGoalController {
             );
     }
 
-    @PostMapping("/{coreGoalId}/sub-goals")
+    @PostMapping("/core-goals/{coreGoalId}/sub-goals")
     public ResponseEntity<ApiResponse<SubGoalCreateResponse, Void>> createSubGoal(
         @PathVariable Long coreGoalId,
         @Valid @RequestBody SubGoalCreateRequest request
@@ -62,4 +65,18 @@ public class SubGoalController {
         return ResponseEntity.created(location)
             .body(ApiResponse.created(response, SUB_GOAL_CREATE_SUCCESS));
     }
+
+    @PatchMapping("/sub-goals/{subGoalId}")
+    public ResponseEntity<ApiResponse<Void, Void>> updateSubGoal(
+        @PathVariable Long subGoalId,
+        @Valid @RequestBody SubGoalUpdateRequest request
+    ) {
+        Long userId = 1L; // TODO: 로그인 구현되면 token에서 사용자id 가져오기
+
+        subGoalCommandService.updateSubGoal(userId, subGoalId, request);
+
+        return ResponseEntity.ok()
+            .body(ApiResponse.ok(SUB_GOAL_UPDATE_SUCCESS));
+    }
+
 }
