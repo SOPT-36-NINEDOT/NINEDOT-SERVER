1ce2104957252eeae92c4a69d2f357c549c20828
kaswhy <sxshx818@naver.com>
2025-07-09 23:24:40 +0900

    :sparkles: feat(#38/mandalart): 만다라트 목표 및 시작 후 며칠째인지 반환 response dto 추가

---
+++ src/main/java/org/sopt36/ninedotserver/mandalart/dto/response/MandalartHistoryResponse.java
@@ -0,0 +1,14 @@
+package org.sopt36.ninedotserver.mandalart.dto.response;
+
+import java.time.LocalDate;
+import org.sopt36.ninedotserver.mandalart.domain.Mandalart;
+
+public record MandalartHistoryResponse(
+    String title,
+    int progressDays
+) {
+
+    public static MandalartHistoryResponse of(Mandalart mandalart, LocalDate today) {
+        return new MandalartHistoryResponse(mandalart.getTitle(), mandalart.getProgressDays(today));
+    }
+}
