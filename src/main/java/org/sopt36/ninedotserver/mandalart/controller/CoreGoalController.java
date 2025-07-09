76b735f96d065d8eacec389d268545e3b2a18d61
kaswhy <sxshx818@naver.com>
2025-07-09 22:10:28 +0900

    :sparkles: feat(#29/mandalrt): 상위 목표 id 리스트 반환 컨트롤러 및 성공 메시지 구현

--- src/main/java/org/sopt36/ninedotserver/mandalart/controller/CoreGoalController.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/controller/CoreGoalController.java
@@ -1,6 +1,7 @@
 package org.sopt36.ninedotserver.mandalart.controller;
 
 import static org.sopt36.ninedotserver.mandalart.controller.message.CoreGoalMessage.CREATED_SUCCESS;
+import static org.sopt36.ninedotserver.mandalart.controller.message.CoreGoalMessage.IDS_RETRIEVED_SUCCESS;
 
 import jakarta.validation.Valid;
 import java.net.URI;
@@ -8,9 +9,11 @@ import lombok.RequiredArgsConstructor;
 import org.sopt36.ninedotserver.global.dto.response.ApiResponse;
 import org.sopt36.ninedotserver.mandalart.dto.request.CoreGoalCreateRequest;
 import org.sopt36.ninedotserver.mandalart.dto.response.CoreGoalCreateResponse;
+import org.sopt36.ninedotserver.mandalart.dto.response.CoreGoalIdsResponse;
 import org.sopt36.ninedotserver.mandalart.service.command.CoreGoalCommandService;
 import org.sopt36.ninedotserver.mandalart.service.query.CoreGoalQueryService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +28,6 @@ public class CoreGoalController {
     private final CoreGoalCommandService coreGoalCommandService;
     private final CoreGoalQueryService coreGoalQueryService;
 
-    // TODO 만다라트 상위 목표 생성
     @PostMapping("/mandalarts/{mandalartId}/core-goals")
     public ResponseEntity<ApiResponse<CoreGoalCreateResponse, Void>> createCoreGoal(
         @PathVariable Long mandalartId,
@@ -42,6 +44,16 @@ public class CoreGoalController {
 
         return ResponseEntity.created(location)
             .body(ApiResponse.created(response, CREATED_SUCCESS));
+    }
+
+    @GetMapping("/mandalarts/{mandalartId}/core-goals")
+    public ResponseEntity<ApiResponse<CoreGoalIdsResponse, Void>> getCoreGoalIds(
+        @PathVariable Long mandalartId
+    ) {
+        Long userId = 2L; // TODO 로그인 구현 완료 후 Authentication 에서 가져오도록 변경
+        CoreGoalIdsResponse response = coreGoalQueryService.getCoreGoalIds(userId, mandalartId);
+
+        return ResponseEntity.ok(ApiResponse.ok(IDS_RETRIEVED_SUCCESS, response));
     }
 
 }
