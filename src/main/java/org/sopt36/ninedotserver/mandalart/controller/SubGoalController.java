f7ea6c209498e842dc7f7c0966b0370aa8c267c8
Jeon Chan Min <chanmin010702@gmail.com>
2025-07-10 01:39:48 +0900

    :sparkles: feat(#36/mandalart): createSubGoal 컨트롤러 생성

--- src/main/java/org/sopt36/ninedotserver/mandalart/controller/SubGoalController.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/controller/SubGoalController.java
@@ -1,11 +1,15 @@
 package org.sopt36.ninedotserver.mandalart.controller;
 
+import static org.sopt36.ninedotserver.mandalart.controller.message.SubGoalMessage.SUB_GOAL_CREATE_SUCCESS;
 import static org.sopt36.ninedotserver.mandalart.controller.message.SubGoalMessage.SUB_GOAL_ID_LIST_FETCH_SUCCESS;
 
+import jakarta.validation.Valid;
+import java.net.URI;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.sopt36.ninedotserver.global.dto.response.ApiResponse;
+import org.sopt36.ninedotserver.mandalart.dto.request.SubGoalCreateRequest;
+import org.sopt36.ninedotserver.mandalart.dto.response.SubGoalCreateResponse;
 import org.sopt36.ninedotserver.mandalart.dto.response.SubGoalIdListResponse;
 import org.sopt36.ninedotserver.mandalart.dto.response.SubGoalIdResponse;
 import org.sopt36.ninedotserver.mandalart.service.command.SubGoalCommandService;
@@ -13,6 +17,8 @@ import org.sopt36.ninedotserver.mandalart.service.query.SubGoalQueryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,9 +38,28 @@ public class SubGoalController {
         List<SubGoalIdResponse> subGoalIds = subGoalQueryService.getSubGoalIds(userId, coreGoalId);
         return ResponseEntity.ok()
             .body(ApiResponse.ok(
-                SUB_GOAL_ID_LIST_FETCH_SUCCESS,
-                SubGoalIdListResponse.from(subGoalIds)
-            )
+                    SUB_GOAL_ID_LIST_FETCH_SUCCESS,
+                    SubGoalIdListResponse.from(subGoalIds)
+                )
+            );
+    }
+
+    @PostMapping("/{coreGoalId}/sub-goals")
+    public ResponseEntity<ApiResponse<SubGoalCreateResponse, Void>> createSubGoal(
+        @PathVariable Long coreGoalId,
+        @Valid @RequestBody SubGoalCreateRequest request
+    ) {
+        Long userId = 1L;
+        SubGoalCreateResponse response = subGoalCommandService.createSubGoal(
+            userId,
+            coreGoalId,
+            request
         );
+
+        URI location = URI.create(
+            "/api/v1/core-goals/" + coreGoalId + "/sub-goals/" + response.id());
+
+        return ResponseEntity.created(location)
+            .body(ApiResponse.created(response, SUB_GOAL_CREATE_SUCCESS));
     }
 }
