847f6bc2594df44ea73f177cc5764e709ce41ece
Jeon Chan Min <chanmin010702@gmail.com>
2025-07-13 05:54:53 +0900

    :sparkles: feat(#61/mandalart): question, choice repository 메서드 추가

--- src/main/java/org/sopt36/ninedotserver/onboarding/repository/QuestionRepository.java
+++ src/main/java/org/sopt36/ninedotserver/onboarding/repository/QuestionRepository.java
@@ -1,5 +1,6 @@
 package org.sopt36.ninedotserver.onboarding.repository;
 
+import java.util.List;
 import org.sopt36.ninedotserver.onboarding.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface QuestionRepository
     extends JpaRepository<Question, Long>, QuestionRepositoryCustom {
+        List<Question> findAllByActivatedTrue();
 
 }
