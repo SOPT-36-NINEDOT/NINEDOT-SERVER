aec9e46d1d56c6e8b1f05ba2b958f0ab2397c5b4
kaswhy <sxshx818@naver.com>
2025-07-12 23:54:26 +0900

    :sparkles: feat(#59/mandalart): 하위 목표 완료 체크 리포지토리 구현

--- src/main/java/org/sopt36/ninedotserver/mandalart/repository/HistoryRepository.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/repository/HistoryRepository.java
@@ -1,5 +1,6 @@
 package org.sopt36.ninedotserver.mandalart.repository;
 
+import java.time.LocalDate;
 import org.sopt36.ninedotserver.mandalart.domain.History;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface HistoryRepository extends JpaRepository<History, Long>, HistoryRepositoryCustom {
 
+    boolean existsBySubGoalSnapshotIdAndCompletedDate(Long subGoalSnapshotId, LocalDate now);
 }
