fb6c481560e435515f7b2f36ea6c805738bd2d7f
Jeon Chan Min <chanmin010702@gmail.com>
2025-07-13 23:45:56 +0900

    :recycle: refactor(#68/ai): import문 정리

--- src/main/java/org/sopt36/ninedotserver/mandalart/repository/MandalartRepository.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/repository/MandalartRepository.java
@@ -1,6 +1,5 @@
 package org.sopt36.ninedotserver.mandalart.repository;
 
-import java.util.Optional;
 import org.sopt36.ninedotserver.mandalart.domain.Mandalart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
