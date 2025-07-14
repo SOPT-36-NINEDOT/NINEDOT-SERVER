b33c58f26e48f37e598111cec3704e6cb022a91d
kaswhy <sxshx818@naver.com>
2025-07-15 07:32:35 +0900

    :sparkles: feat(#91/mandalart): 만다라트 보드 전체 조회 api 컨트롤러, 성공 메시지 구현

--- src/main/java/org/sopt36/ninedotserver/mandalart/controller/MandalartController.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/controller/MandalartController.java
@@ -1,6 +1,7 @@
 package org.sopt36.ninedotserver.mandalart.controller;
 
 import static org.sopt36.ninedotserver.mandalart.controller.message.MandalartMessage.CREATED_SUCCESS;
+import static org.sopt36.ninedotserver.mandalart.controller.message.MandalartMessage.MANDALART_BOARD_RETRIEVED_SUCCESS;
 import static org.sopt36.ninedotserver.mandalart.controller.message.MandalartMessage.MANDALART_RETRIEVED_SUCCESS;
 import static org.sopt36.ninedotserver.mandalart.controller.message.MandalartMessage.PROGRESS_HISTORY_RETRIEVED_SUCCESS;
 
@@ -9,6 +10,7 @@ import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.sopt36.ninedotserver.global.dto.response.ApiResponse;
 import org.sopt36.ninedotserver.mandalart.dto.request.MandalartCreateRequest;
+import org.sopt36.ninedotserver.mandalart.dto.response.MandalartBoardResponse;
 import org.sopt36.ninedotserver.mandalart.dto.response.MandalartCreateResponse;
 import org.sopt36.ninedotserver.mandalart.dto.response.MandalartHistoryResponse;
 import org.sopt36.ninedotserver.mandalart.dto.response.MandalartResponse;
@@ -43,7 +45,7 @@ public class MandalartController {
         URI location = URI.create("/api/v1/mandalarts/" + mandalartId);
 
         return ResponseEntity.created(location)
-                   .body(ApiResponse.created(response, CREATED_SUCCESS));
+            .body(ApiResponse.created(response, CREATED_SUCCESS));
     }
 
     @GetMapping("/mandalarts/{mandalartId}/histories")
@@ -65,5 +67,16 @@ public class MandalartController {
         Long userId = 1L;
         MandalartResponse response = mandalartQueryService.getMandalart(userId, mandalartId);
         return ResponseEntity.ok(ApiResponse.ok(MANDALART_RETRIEVED_SUCCESS, response));
+    }
+
+    @GetMapping("/mandalarts/{mandalartId}/board")
+    public ResponseEntity<ApiResponse<MandalartBoardResponse, Void>> getMandalartBoard(
+        @PathVariable Long mandalartId
+    ) {
+        Long userId = 1L;
+        MandalartBoardResponse response = mandalartQueryService
+            .getMandalartBoard(userId, mandalartId);
+
+        return ResponseEntity.ok(ApiResponse.ok(MANDALART_BOARD_RETRIEVED_SUCCESS, response));
     }
 }
