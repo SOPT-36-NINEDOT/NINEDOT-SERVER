07b0b5b41ef204490d29e154d2d0c6f40d0c436a
kaswhy <sxshx818@naver.com>
2025-07-15 17:29:01 +0900

    :sparkles: feat(#87/mandalart): 하위 목표 ai 추천값 만다라트에 추가 컨트롤러, 성공 메시지 구현

--- src/main/java/org/sopt36/ninedotserver/mandalart/controller/message/SubGoalMessage.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/controller/message/SubGoalMessage.java
@@ -8,4 +8,5 @@ public class SubGoalMessage {
     public static final String SUB_GOAL_UPDATE_SUCCESS = "성공적으로 해당 하위 목표의 내용을 수정했습니다.";
     public static final String SUB_GOAL_DELETE_SUCCESS = "성공적으로 해당 하위 목표의 내용을 삭제했습니다.";
     public static final String SUB_GOAL_AI_RECOMMENDATION_SUCCESS = "하위 목표 AI 추천이 완료되었습니다.";
+    public static final String SUB_GOAL_AI_CREATED_SUCCESS = "성공적으로 AI 추천 목표들을 만다라트의 하위 목표에 저장했습니다.";
 }
