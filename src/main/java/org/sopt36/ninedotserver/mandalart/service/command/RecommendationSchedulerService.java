88b6c67c8d266d6e7184493df361dfd7a546ffb9
kaswhy <sxshx818@naver.com>
2025-07-17 03:22:48 +0900

    :fire: hotfix(#114/mandalart): 서버 시간대 설정 및 스케쥴러 시간대 설정

--- src/main/java/org/sopt36/ninedotserver/mandalart/service/command/RecommendationSchedulerService.java
+++ src/main/java/org/sopt36/ninedotserver/mandalart/service/command/RecommendationSchedulerService.java
@@ -39,7 +39,7 @@ public class RecommendationSchedulerService {
     private final MandalartRepository mandalartRepository;
     private final HistoryRepository historyRepository;
 
-    @Scheduled(cron = "0 0 3 * * *")
+    @Scheduled(cron = "0 22 3 * * *", zone = "Asia/Seoul")
     @Transactional
     public void generateDailyRecommendations() {
         LocalDate today = LocalDate.now();
