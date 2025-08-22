cabd5ac596850ba7c3acbd88f278d410126d8b46
kaswhy <sxshx818@naver.com>
2025-08-22 15:06:57 +0900

    :sparkles: feat(#162/.github): 빌드, 테스트 분리 및 테스트 환경 기본값 재설정

--- src/test/java/org/sopt36/ninedotserver/NinedotServerApplicationTests.java
+++ src/test/java/org/sopt36/ninedotserver/NinedotServerApplicationTests.java
@@ -2,7 +2,9 @@ package org.sopt36.ninedotserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class NinedotServerApplicationTests {
 
