6ef885a52d6f14a71300b6663312baa9fbfbe4cc
kaswhy <sxshx818@naver.com>
2025-08-30 15:43:10 +0900

    :art: style(#267/onboarding): onboarding 도메인 코드 내부 공백 정리

--- src/main/java/org/sopt36/ninedotserver/onboarding/repository/QuestionRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package org.sopt36.ninedotserver.onboarding.repository;
-
-import java.util.List;
-import org.sopt36.ninedotserver.onboarding.domain.Question;
-
-public interface QuestionRepositoryCustom {
-    List<Question> findAllActivated();
-}
