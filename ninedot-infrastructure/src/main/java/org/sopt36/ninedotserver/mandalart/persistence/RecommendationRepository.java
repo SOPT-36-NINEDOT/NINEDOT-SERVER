73e12445ac28ae643cb46500e8095331fe4556eb
kaswhy <sxshx818@naver.com>
2025-09-01 22:48:18 +0900

    :recycle: refactor(#168/mandalart): mandalart 리포지토리 관련 클래스 ninedot-infrastructure 모듈로 이동

--- ninedot-infrastructure/src/main/java/org/sopt36/ninedotserver/mandalart/persistence/RecommendationRepository.java
+++ ninedot-infrastructure/src/main/java/org/sopt36/ninedotserver/mandalart/persistence/RecommendationRepository.java
@@ -1,13 +1,15 @@
-package org.sopt36.ninedotserver.mandalart.repository;
+package org.sopt36.ninedotserver.mandalart.persistence;
 
 import java.time.LocalDate;
 import java.util.List;
-import org.sopt36.ninedotserver.mandalart.domain.Recommendation;
+import org.sopt36.ninedotserver.mandalart.model.Recommendation;
+import org.sopt36.ninedotserver.mandalart.port.out.RecommendationRepositoryPort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
+public interface RecommendationRepository
+    extends JpaRepository<Recommendation, Long>, RecommendationRepositoryPort {
 
     List<Recommendation> findAllBySubGoalSnapshot_SubGoal_CoreGoal_Mandalart_IdAndRecommendationDate(
         Long mandalartId,
