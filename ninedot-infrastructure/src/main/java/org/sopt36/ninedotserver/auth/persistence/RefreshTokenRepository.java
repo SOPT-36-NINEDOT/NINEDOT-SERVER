33f64e18ba7fe1fb6911d46f96007be55e2e981d
kaswhy <sxshx818@naver.com>
2025-09-01 22:25:35 +0900

    :recycle: refactor(#168/auth): auth 리포지토리 관련 클래스 ninedot-infrastructure 모듈로 이동

--- ninedot-infrastructure/src/main/java/org/sopt36/ninedotserver/auth/persistence/RefreshTokenRepository.java
+++ ninedot-infrastructure/src/main/java/org/sopt36/ninedotserver/auth/persistence/RefreshTokenRepository.java
@@ -1,14 +1,15 @@
-package org.sopt36.ninedotserver.auth.repository;
+package org.sopt36.ninedotserver.auth.persistence;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
-import org.sopt36.ninedotserver.auth.domain.RefreshToken;
+import org.sopt36.ninedotserver.auth.model.RefreshToken;
+import org.sopt36.ninedotserver.auth.port.out.RefreshTokenRepositoryPort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RefreshTokenRepository
-    extends JpaRepository<RefreshToken, Long>, RefreshTokenRepositoryCustom {
+    extends JpaRepository<RefreshToken, Long>, RefreshTokenRepositoryPort {
 
     Optional<RefreshToken> findByRefreshTokenAndExpiresAtAfter(
         String refreshToken,
