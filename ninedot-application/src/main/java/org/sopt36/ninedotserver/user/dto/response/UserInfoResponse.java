4ac4e15b08a960033419fa7380fb7d90c60add33
kaswhy <sxshx818@naver.com>
2025-09-01 22:44:47 +0900

    :recycle: refactor(#168/user): user 관련 서비, dto 클래스 ninedot-application 모듈로 이동

--- ninedot-application/src/main/java/org/sopt36/ninedotserver/user/dto/response/UserInfoResponse.java
+++ ninedot-application/src/main/java/org/sopt36/ninedotserver/user/dto/response/UserInfoResponse.java
@@ -1,6 +1,6 @@
 package org.sopt36.ninedotserver.user.dto.response;
 
-import org.sopt36.ninedotserver.user.domain.User;
+import org.sopt36.ninedotserver.user.model.User;
 
 public record UserInfoResponse(
     Long id,
--- ninedot-application/src/main/java/org/sopt36/ninedotserver/user/usecase/UserQueryService.java
+++ ninedot-application/src/main/java/org/sopt36/ninedotserver/user/usecase/UserQueryService.java
@@ -1,12 +1,12 @@
-package org.sopt36.ninedotserver.user.service.query;
+package org.sopt36.ninedotserver.user.usecase;
 
 import static org.sopt36.ninedotserver.user.exception.UserErrorCode.USER_NOT_FOUND;
 
 import lombok.RequiredArgsConstructor;
-import org.sopt36.ninedotserver.user.domain.User;
 import org.sopt36.ninedotserver.user.dto.response.UserInfoResponse;
 import org.sopt36.ninedotserver.user.exception.UserException;
-import org.sopt36.ninedotserver.user.repository.UserRepository;
+import org.sopt36.ninedotserver.user.model.User;
+import org.sopt36.ninedotserver.user.port.out.UserRepositoryPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserQueryService {
 
-    private final UserRepository userRepository;
+    private final UserRepositoryPort userRepository;
 
     public UserInfoResponse getUserInfo(Long userId) {
         User user = userRepository.findById(userId)
