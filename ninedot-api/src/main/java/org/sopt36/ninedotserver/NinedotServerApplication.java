cf303288ab654a9c696082b63742b8c3cbb22e31
kaswhy <sxshx818@naver.com>
2025-09-09 23:21:42 +0900

    :recycle: refactor(#175/auth): 로그인 관련 필요한 properties 분리

--- ninedot-api/src/main/java/org/sopt36/ninedotserver/NinedotServerApplication.java
+++ ninedot-api/src/main/java/org/sopt36/ninedotserver/NinedotServerApplication.java
@@ -2,8 +2,10 @@ package org.sopt36.ninedotserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ConfigurationPropertiesScan(basePackages = "org.sopt36.ninedotserver")
 @EnableScheduling
 @SpringBootApplication
 public class NinedotServerApplication {
