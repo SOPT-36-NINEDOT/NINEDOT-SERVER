0ff7bef09369f3edec69fb68a35427e4bca06daa
kaswhy <sxshx818@naver.com>
2025-09-10 01:18:41 +0900

    :recycle: refactor(#175/auth): redirect uri 검증 로직 구현

--- ninedot-infrastructure/src/main/java/org/sopt36/ninedotserver/auth/adapter/out/redirect/RedirectUriWhitelistValidator.java
+++ ninedot-infrastructure/src/main/java/org/sopt36/ninedotserver/auth/adapter/out/redirect/RedirectUriWhitelistValidator.java
@@ -1,5 +1,25 @@
 package org.sopt36.ninedotserver.auth.adapter.out.redirect;
 
-public class RedirectUriWhitelistValidator {
+import java.util.List;
+import org.sopt36.ninedotserver.auth.port.out.policy.RedirectUriValidationPort;
+import org.springframework.stereotype.Component;
 
+@Component
+public class RedirectUriWhitelistValidator implements RedirectUriValidationPort {
+
+    private final List<String> whitelist;
+    private final String defaultRedirectUri;
+
+    public RedirectUriWhitelistValidator(RedirectProperties redirectProperties) {
+        this.whitelist = redirectProperties.getWhitelist();
+        this.defaultRedirectUri = redirectProperties.getDefaultRedirectUri();
+    }
+
+    @Override
+    public String resolveAndValidate(String clientProvidedRedirectUri) {
+        if (clientProvidedRedirectUri != null && whitelist.contains(clientProvidedRedirectUri)) {
+            return clientProvidedRedirectUri;
+        }
+        return defaultRedirectUri;
+    }
 }
