212cb13fb3af10dc132b09ca7f07906822cc9cb6
jeonchanmin <chanmin010702@gmail.com>
2025-09-18 03:36:54 +0900

    :recycle: refactor(#173/user): User VO 적용

---
+++ ninedot-domain/src/main/java/org/sopt36/ninedotserver/user/model/value/Birthday.java
@@ -0,0 +1,61 @@
+package org.sopt36.ninedotserver.user.model.value;
+
+import org.sopt36.ninedotserver.user.exception.UserErrorCode;
+import org.sopt36.ninedotserver.user.exception.UserException;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public record Birthday(String value) {
+
+    public static final int MAX_LENGTH = 20;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+
+
+    public Birthday {
+        validateNotBlank(value);
+        validateLength(value);
+        validateDateFormat(value);
+    }
+
+    private static void validateDateFormat(String value) {
+        try {
+            LocalDate.parse(value, FORMATTER);
+        } catch (DateTimeParseException e) {
+            throw new UserException(UserErrorCode.INVALID_BIRTHDAY_TYPE);
+        }
+    }
+
+    private static void validateLength(String value) {
+        if (value.length() > MAX_LENGTH) {
+            throw new UserException(UserErrorCode.INVALID_BIRTHDAY_LENGTH);
+        }
+    }
+
+    private static void validateNotBlank(String value) {
+        if (value == null || value.isBlank()) {
+            throw new UserException(UserErrorCode.BIRTHDAY_NOT_BLANK);
+        }
+    }
+
+    public int calculateAge() {
+        return calculateAge(LocalDate.now());
+    }
+
+    public int calculateAge(Clock clock) {
+        return calculateAge(LocalDate.now(clock));
+    }
+
+    private int calculateAge(LocalDate today) {
+        LocalDate birthDate = LocalDate.parse(this.value, FORMATTER);
+        return Period.between(birthDate, today).getYears();
+    }
+
+    public LocalDate toDate() {
+        return LocalDate.parse(value, FORMATTER);
+    }
+
+}
---
+++ ninedot-domain/src/main/java/org/sopt36/ninedotserver/user/model/value/ProfileImageUrl.java
@@ -0,0 +1,28 @@
+package org.sopt36.ninedotserver.user.model.value;
+
+import org.sopt36.ninedotserver.user.exception.UserErrorCode;
+import org.sopt36.ninedotserver.user.exception.UserException;
+
+public record ProfileImageUrl(String value) {
+
+    public static final int MAX_LENGTH = 1000;
+
+    public ProfileImageUrl {
+        validateNotBlank(value);
+        validateLength(value);
+    }
+
+    private void validateLength(String value) {
+        if (value.length() > MAX_LENGTH) {
+            throw new UserException(UserErrorCode.PROFILE_IMAGE_URL_TOO_LONG);
+        }
+    }
+
+    private void validateNotBlank(String value) {
+        if (value == null || value.isBlank()) {
+            throw new UserException(UserErrorCode.PROFILE_IMAGE_NOT_BLANK);
+        }
+    }
+
+
+}
