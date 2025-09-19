1d9e71a0b442c5db489aac4a9a28bb959c0db990
jeonchanmin <chanmin010702@gmail.com>
2025-09-19 11:52:36 +0900

    :recycle: refactor(#173/user): 추가된 dto들과 mapper 적용

--- ninedot-api/src/main/java/org/sopt36/ninedotserver/user/v1/UserController.java
+++ ninedot-api/src/main/java/org/sopt36/ninedotserver/user/v1/UserController.java
@@ -1,30 +1,35 @@
 package org.sopt36.ninedotserver.user.v1;
 
-import static org.sopt36.ninedotserver.user.v1.message.UserMessage.USER_INFO_RETRIEVED_SUCCESS;
-
 import lombok.RequiredArgsConstructor;
 import org.sopt36.ninedotserver.dto.response.ApiResponse;
-import org.sopt36.ninedotserver.user.dto.response.UserInfoResponse;
-import org.sopt36.ninedotserver.user.usecase.UserQueryService;
+import org.sopt36.ninedotserver.user.dto.query.GetUserInfoQuery;
+import org.sopt36.ninedotserver.user.usecase.port.in.GetUserInfoUseCase;
+import org.sopt36.ninedotserver.user.v1.dto.response.UserInfoResponse;
+import org.sopt36.ninedotserver.user.v1.mapper.UserResponseMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.sopt36.ninedotserver.user.v1.message.UserMessage.USER_INFO_RETRIEVED_SUCCESS;
+
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @RestController
 public class UserController {
 
-    private final UserQueryService userQueryService;
+    private final GetUserInfoUseCase getUserInfoUseCase;
+    private final UserResponseMapper userResponseMapper;
 
     @GetMapping("/users/info")
     public ResponseEntity<ApiResponse<UserInfoResponse, Void>> getUserInfo(
-        Authentication authentication
+            Authentication authentication
     ) {
         Long userId = Long.parseLong(authentication.getName());
-        UserInfoResponse response = userQueryService.getUserInfo(userId);
+        var query = new GetUserInfoQuery(userId);
+        var result = getUserInfoUseCase.getUserInfo(query);
+        var response = userResponseMapper.toResponse(result);
         return ResponseEntity.ok(ApiResponse.ok(USER_INFO_RETRIEVED_SUCCESS, response));
     }
 
