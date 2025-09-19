f432bd1d791bbf148ac27471dc350edd2dd50570
jeonchanmin <chanmin010702@gmail.com>
2025-09-19 11:47:26 +0900

    :recycle: refactor(#173/user): result dto 추가

---
+++ ninedot-application/src/main/java/org/sopt36/ninedotserver/user/usecase/handler/GetUserInfoHandler.java
@@ -0,0 +1,28 @@
+package org.sopt36.ninedotserver.user.usecase.handler;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt36.ninedotserver.user.dto.query.GetUserInfoQuery;
+import org.sopt36.ninedotserver.user.dto.result.UserInfoResult;
+import org.sopt36.ninedotserver.user.exception.UserErrorCode;
+import org.sopt36.ninedotserver.user.exception.UserException;
+import org.sopt36.ninedotserver.user.model.User;
+import org.sopt36.ninedotserver.user.port.out.UserQueryPort;
+import org.sopt36.ninedotserver.user.usecase.port.in.GetUserInfoUseCase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class GetUserInfoHandler implements GetUserInfoUseCase {
+
+    private final UserQueryPort userRepository;
+
+    @Override
+    public UserInfoResult getUserInfo(GetUserInfoQuery query) {
+        User user = userRepository.findById(query.userId())
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+
+        return UserInfoResult.from(user);
+    }
+}
