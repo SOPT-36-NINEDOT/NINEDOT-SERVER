9feb9eee55bec8ac09925bd1b1dbfc48608d483b
jeonchanmin <chanmin010702@gmail.com>
2025-09-21 14:44:27 +0900

    :recycle: refactor(#173/user): UseCase 메서드 명 excute로 통일

--- ninedot-application/src/main/java/org/sopt36/ninedotserver/user/port/in/GetUserInfoUseCase.java
+++ ninedot-application/src/main/java/org/sopt36/ninedotserver/user/port/in/GetUserInfoUseCase.java
@@ -1,8 +1,8 @@
-package org.sopt36.ninedotserver.user.usecase.port.in;
+package org.sopt36.ninedotserver.user.port.in;
 
 import org.sopt36.ninedotserver.user.dto.query.UserInfoQuery;
 import org.sopt36.ninedotserver.user.dto.result.UserInfoResult;
 
 public interface GetUserInfoUseCase {
-    UserInfoResult getUserInfo(UserInfoQuery query);
+    UserInfoResult execute(UserInfoQuery query);
 }
--- ninedot-application/src/main/java/org/sopt36/ninedotserver/user/service/GetUserInfoHandler.java
+++ ninedot-application/src/main/java/org/sopt36/ninedotserver/user/service/GetUserInfoHandler.java
@@ -1,4 +1,4 @@
-package org.sopt36.ninedotserver.user.usecase.handler;
+package org.sopt36.ninedotserver.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt36.ninedotserver.user.dto.query.UserInfoQuery;
@@ -7,7 +7,7 @@ import org.sopt36.ninedotserver.user.exception.UserErrorCode;
 import org.sopt36.ninedotserver.user.exception.UserException;
 import org.sopt36.ninedotserver.user.model.User;
 import org.sopt36.ninedotserver.user.port.out.UserQueryPort;
-import org.sopt36.ninedotserver.user.usecase.port.in.GetUserInfoUseCase;
+import org.sopt36.ninedotserver.user.port.in.GetUserInfoUseCase;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +19,7 @@ public class GetUserInfoHandler implements GetUserInfoUseCase {
     private final UserQueryPort userQueryPort;
 
     @Override
-    public UserInfoResult getUserInfo(UserInfoQuery query) {
+    public UserInfoResult execute(UserInfoQuery query) {
         User user = userQueryPort.findById(query.userId())
                 .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
 
