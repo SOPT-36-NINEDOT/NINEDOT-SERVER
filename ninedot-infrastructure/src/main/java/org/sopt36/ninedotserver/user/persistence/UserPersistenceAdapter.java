7f4e258a655153cc7ca999547424a938673e0f1e
jeonchanmin <chanmin010702@gmail.com>
2025-09-21 15:06:47 +0900

    :recycle: refactor(#173/user): userRepository -> UserRepository로 클래스명 오타 수정

--- ninedot-infrastructure/src/main/java/org/sopt36/ninedotserver/user/persistence/UserPersistenceAdapter.java
+++ ninedot-infrastructure/src/main/java/org/sopt36/ninedotserver/user/persistence/UserPersistenceAdapter.java
@@ -2,7 +2,7 @@ package org.sopt36.ninedotserver.user.persistence;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt36.ninedotserver.user.model.User;
-import org.sopt36.ninedotserver.user.persistence.jpa.repository.userRepository;
+import org.sopt36.ninedotserver.user.persistence.jpa.repository.UserRepository;
 import org.sopt36.ninedotserver.user.port.out.UserCommandPort;
 import org.sopt36.ninedotserver.user.port.out.UserQueryPort;
 import org.springframework.stereotype.Repository;
@@ -14,7 +14,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserPersistenceAdapter implements UserQueryPort, UserCommandPort {
 
-    private final userRepository userRepository;
+    private final UserRepository userRepository;
 
 
     @Override
--- ninedot-infrastructure/src/main/java/org/sopt36/ninedotserver/user/persistence/jpa/repository/UserRepository.java
+++ ninedot-infrastructure/src/main/java/org/sopt36/ninedotserver/user/persistence/jpa/repository/UserRepository.java
@@ -3,5 +3,5 @@ package org.sopt36.ninedotserver.user.persistence.jpa.repository;
 import org.sopt36.ninedotserver.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface userRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
