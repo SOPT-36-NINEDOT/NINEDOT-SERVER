19813e9bb50bb6ad06fb13941ffb1c244438e356
jeonchanmin <chanmin010702@gmail.com>
2025-09-23 02:13:52 +0900

    :recycle: refactor(#188/user): Email VO에 이메일 형식 검증 추가

--- ninedot-domain/src/main/java/org/sopt36/ninedotserver/user/model/value/Email.java
+++ ninedot-domain/src/main/java/org/sopt36/ninedotserver/user/model/value/Email.java
@@ -3,14 +3,17 @@ package org.sopt36.ninedotserver.user.model.value;
 import org.sopt36.ninedotserver.user.exception.UserErrorCode;
 import org.sopt36.ninedotserver.user.exception.UserException;
 
+import java.util.regex.Pattern;
+
 public record Email(String value) {
 
     public static final int MAX_LENGTH = 255;
-
+    private static final Pattern EMAIL_REGEX = Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
 
     public Email {
         validateNotBlank(value);
         validateLength(value);
+        validateFormat(value);
     }
 
     private static void validateLength(String value) {
@@ -22,6 +25,12 @@ public record Email(String value) {
     private static void validateNotBlank(String value) {
         if (value == null || value.isBlank()) {
             throw new UserException(UserErrorCode.EMAIL_NOT_BLANK);
+        }
+    }
+
+    private static void validateFormat(String value) {
+        if (!EMAIL_REGEX.matcher(value).matches()) {
+            throw new UserException(UserErrorCode.INVALID_EMAIL);
         }
     }
 
