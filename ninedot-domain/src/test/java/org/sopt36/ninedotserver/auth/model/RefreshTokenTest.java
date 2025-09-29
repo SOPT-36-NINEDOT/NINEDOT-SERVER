ff69ed7db9b481ce2d775887a519da1205cf39cf
kaswhy <sxshx818@naver.com>
2025-09-29 18:50:13 +0900

    :white_check_mark: test(#182/auth): refreshToken rotate, getUserId()에 대한 도메인 테스트 코드 작성

--- ninedot-domain/src/test/java/org/sopt36/ninedotserver/auth/model/RefreshTokenTest.java
+++ ninedot-domain/src/test/java/org/sopt36/ninedotserver/auth/model/RefreshTokenTest.java
@@ -1,8 +1,9 @@
 package org.sopt36.ninedotserver.auth.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -16,7 +17,7 @@ class RefreshTokenTest {
         // given
         User user = Mockito.mock(User.class);
         String refreshToken = "refreshtoken.header.payload.signature";
-        LocalDateTime expiresAt = LocalDateTime.now().plusDays(7);
+        Instant expiresAt = Instant.now().plusMillis(1209600000);
 
         // when
         RefreshToken refreshTokenDomain = RefreshToken.create(user, refreshToken, expiresAt);
@@ -27,5 +28,48 @@ class RefreshTokenTest {
         assertThat(refreshTokenDomain.getRefreshToken()).isEqualTo(refreshToken);
         assertThat(refreshTokenDomain.getExpiresAt()).isEqualTo(expiresAt);
     }
+
+    @Test
+    @DisplayName("rotate(refreshToken, expiresAt): 새로운 RefreshToken으로 회전한다.")
+    void rotate_refresh_token() {
+        // given
+        User user = Mockito.mock(User.class);
+        String refreshToken = "refreshtoken.header.payload.signature";
+        Instant expiresAt = Instant.now().plusMillis(1209600000);
+
+        RefreshToken refreshTokenDomain = RefreshToken.create(user, refreshToken, expiresAt);
+
+        String newRefreshToken = "newrefreshtoken.header.payload.signature";
+        Instant newExpiresAt = Instant.now().plusMillis(1209600001);
+
+        // when
+        refreshTokenDomain.rotate(newRefreshToken, newExpiresAt);
+
+        // then
+        assertThat(refreshTokenDomain.getUser()).isSameAs(user);
+        assertThat(refreshTokenDomain.getRefreshToken()).isEqualTo(newRefreshToken);
+        assertThat(refreshTokenDomain.getExpiresAt()).isEqualTo(newExpiresAt);
+    }
+
+    @Test
+    @DisplayName("getUserId(): User의 Id를 반환한다.")
+    void getUserId_returnsUserId() {
+        // given
+        User mockUser = Mockito.mock(User.class);
+        when(mockUser.getId()).thenReturn(1L);
+
+        RefreshToken refreshTokenDomain = RefreshToken.create(
+            mockUser,
+            "refreshtoken.string",
+            Instant.now()
+        );
+
+        // when
+        Long actualUserId = refreshTokenDomain.getUserId();
+
+        // then
+        assertThat(actualUserId).isEqualTo(1L);
+    }
+
 
 }
