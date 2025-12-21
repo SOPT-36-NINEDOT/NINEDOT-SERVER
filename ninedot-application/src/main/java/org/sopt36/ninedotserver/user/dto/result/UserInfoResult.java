9b255a8c620c7d62e5bc1cd9d77e9ea04cd29a38
jeonchanmin <chanmin010702@gmail.com>
2025-12-21 21:56:46 +0900

    :recycle: refactor(#219/user): UserInfoResult에 온보딩 상태 필드 추가

--- ninedot-application/src/main/java/org/sopt36/ninedotserver/user/dto/result/UserInfoResult.java
+++ ninedot-application/src/main/java/org/sopt36/ninedotserver/user/dto/result/UserInfoResult.java
@@ -1,19 +1,28 @@
 package org.sopt36.ninedotserver.user.dto.result;
 
+import org.sopt36.ninedotserver.auth.model.OnboardingPage;
+import org.sopt36.ninedotserver.auth.service.login.dto.OnboardingStatus;
 import org.sopt36.ninedotserver.user.model.User;
 
 public record UserInfoResult(
         Long id,
         String name,
         String email,
-        String profileImageUrl
+        String profileImageUrl,
+        boolean onboardingCompleted,
+        OnboardingPage nextPage
 ) {
-    public static UserInfoResult from(User user) {
+    public static UserInfoResult from(
+            User user,
+            OnboardingStatus onboardingStatus
+            ) {
         return new UserInfoResult(
                 user.getId(),
                 user.nameAsString(),
                 user.emailAsString(),
-                user.profileImageUrlAsString()
+                user.profileImageUrlAsString(),
+                onboardingStatus.onboardingCompleted(),
+                onboardingStatus.nextPage()
         );
     }
 }
